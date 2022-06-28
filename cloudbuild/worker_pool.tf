4145d5282352c5d4207bd9932ea1e8e8511c9c32
Ahmed Belgana <belgana@google.com>
2022-06-28 14:34:22 -0400

    Troubleshooting

--- cloudbuild/worker_pool.tf
+++ cloudbuild/worker_pool.tf
@@ -1,15 +1,15 @@
 resource "google_cloudbuild_worker_pool" "private_pool" {
-  provider = google-beta
+  name = "default-private-pool-local"
 
-  project  = data.tfe_outputs.foundation.values.svc_project_id
-  name     = var.name
-  location = var.region
+  project  = "svc-project-25e0"
+  location = "us-east4"
   worker_config {
-    disk_size_gb = 100
-    machine_type = "e2-standard-4"
-    // no_external_ip = true
+    disk_size_gb   = 100
+    machine_type   = "e2-standard-4"
+    no_external_ip = true
   }
+
   network_config {
-    peered_network = data.tfe_outputs.foundation.values.vpc_id
+    peered_network = "projects/host-project-013e/global/networks/shared-vpc"
   }
 }
