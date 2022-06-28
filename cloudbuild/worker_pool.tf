1132756551506a4cc63d68c8675fcdc73720b71d
Ahmed Belgana <belgana@google.com>
2022-06-28 14:12:15 -0400

    Make the node pool public

--- cloudbuild/worker_pool.tf
+++ cloudbuild/worker_pool.tf
@@ -5,11 +5,11 @@ resource "google_cloudbuild_worker_pool" "private_pool" {
   name     = var.name
   location = var.region
   worker_config {
-    disk_size_gb   = 100
-    machine_type   = "e2-standard-4"
-    no_external_ip = true
+    disk_size_gb = 100
+    machine_type = "e2-standard-4"
+    // no_external_ip = true
   }
-  // network_config {
-  //   peered_network = "projects/${data.tfe_outputs.foundation.values.host_project_number}/global/networks/${data.tfe_outputs.foundation.values.vpc_name}"
-  // }
+  network_config {
+    peered_network = data.tfe_outputs.foundation.values.vpc_id
+  }
 }
