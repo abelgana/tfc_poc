41b5fafa9f031af00f5981144ba1cc05418e4548
Ahmed Belgana <belgana@google.com>
2022-06-29 11:32:34 -0400

    Update GKE version

--- gke/variables.tf
+++ gke/variables.tf
@@ -24,7 +24,7 @@ variable "region" {
 variable "kubernetes_version" {
   type        = string
   description = "The Kubernetes version of the masters. If set to 'latest' it will pull latest available version in the selected region."
-  default     = "1.21.10-gke.2000"
+  default     = "1.21.11-gke.1900"
 }
 
 variable "master_authorized_networks" {
