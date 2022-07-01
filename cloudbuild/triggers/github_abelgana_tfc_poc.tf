8ec8490a8d82abb70828c6d64ba56fd03ae748fc
Ahmed Belgana <belgana@google.com>
2022-07-01 09:34:25 -0400

    Try build-trigger

--- cloudbuild/triggers/github_abelgana_tfc_poc.tf
+++ cloudbuild/triggers/github_abelgana_tfc_poc.tf
@@ -1,10 +1,32 @@
-resource "google_cloudbuild_trigger" "filename-trigger" {
-  project = data.tfe_outputs.foundation.values.svc_project_id
+// resource "google_cloudbuild_trigger" "filename-trigger" {
+//   project = data.tfe_outputs.foundation.values.svc_project_id
+//
+//   trigger_template {
+//     branch_name = "main"
+//     repo_name   = "github_abelgana_tfc_poc"
+//   }
+//
+//   filename = "postgresql/cloudbuild.yaml"
+// }
 
+resource "google_cloudbuild_trigger" "build-trigger" {
+  project = data.tfe_outputs.foundation.values.svc_project_id
   trigger_template {
     branch_name = "main"
     repo_name   = "github_abelgana_tfc_poc"
   }
 
-  filename = "postgresql/cloudbuild.yaml"
+  build {
+    step {
+      name    = "shell"
+      args    = ["echo", "I did run"]
+      timeout = "120s"
+    }
+
+    options {
+      dynamic_substitutions = true
+      log_streaming_option  = "STREAM_OFF"
+      worker_pool           = "projects/$PROJECT_ID/locations/us-east4/workerPools/private-pool"
+    }
+  }
 }
