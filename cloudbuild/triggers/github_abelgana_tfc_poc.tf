490f2958260ddff776f003193d4c889bff2d2be8
Ahmed Belgana <belgana@google.com>
2022-07-01 09:22:33 -0400

    Add project

--- cloudbuild/triggers/github_abelgana_tfc_poc.tf
+++ cloudbuild/triggers/github_abelgana_tfc_poc.tf
@@ -1,4 +1,6 @@
 resource "google_cloudbuild_trigger" "filename-trigger" {
+  project = data.tfe_outputs.foundation.values.svc_project_id
+
   trigger_template {
     branch_name = "main"
     repo_name   = "github_abelgana_tfc_poc"
