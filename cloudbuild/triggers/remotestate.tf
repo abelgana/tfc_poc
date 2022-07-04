1b724c374a445565d7f31ac574d3eb61fdc2959f
Ahmed Belgana <belgana@google.com>
2022-07-04 14:54:21 -0400

    Clean

--- cloudbuild/triggers/remotestate.tf
+++ cloudbuild/triggers/remotestate.tf
@@ -2,3 +2,4 @@ data "tfe_outputs" "foundation" {
   organization = "belgana"
   workspace    = "tfc_poc_foundation"
 }
+
--- postgresql/providers.tf
+++ postgresql/providers.tf
@@ -1,9 +1,11 @@
 provider "google" {
+  project      = data.tfe_outputs.foundation.values.svc_project_id
   access_token = var.access_token
   region       = var.region
 }
 
 provider "google-beta" {
+  project      = data.tfe_outputs.foundation.values.svc_project_id
   access_token = var.access_token
   region       = var.region
 }
